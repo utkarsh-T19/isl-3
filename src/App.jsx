2e64b62b924d3607adaa96a19f7e961a28d2ed20
utkarsh.tawakley <utkarsh.tawakley@idfy.com>
2026-04-11 00:39:23 +0530

    feat: add github action to deploy using github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
@@ -7,7 +7,7 @@ import Fixtures from './pages/Fixtures';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -15,7 +15,7 @@ function App() {
           <Route path="fixtures" element={<Fixtures />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
